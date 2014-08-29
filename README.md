0a5416cadf59320b69d86cb88edbbcf7bcd84367
jpowell127 <jpowell.cr@gmail.com>
2014-08-28 23:17:47 -0500

    Changed Approach
    Ditched bootstrap dependency, rewrote slider functionality

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-sweetCarousel
-=============
+## Synopsis
 
-Sweet New jQuery Carousel Plugin
+Sweet Carousel is a free jQuery plugin that lets you create beautiful responsive image carousels! A sweet set of images have been provided for your convenience served up from the Amazon CloudFront. For easy customization, you can also pass in your own images
+
+## Code Example
+
+## Installation
+
+Provide code examples and explanations of how to get the project.
--- js/jquery.sweetCarousel.js
+++ js/jquery.sweetCarousel.js
@@ -1,6 +1,6 @@
 (function($) {
     // Ajax request to load, compile, and append Handlebars template to the DOM
-    function getTemplateAjax(path,data) {
+    function getTemplateAjax(path,data,div) {
         var template;
 
         $.ajax({
@@ -8,61 +8,94 @@
             cache: true,
             success: function(result) {
                 template  = Handlebars.compile(result);
-                $('#mySweetCarousel').html(template(data));
-                $(".item").first().addClass("active");
-                $(".carousel-indicators li").first().addClass("active");
+                $(div).html(template(data));
             }
-        });
+        }).done(function( msg ) {
+
+			var carouselUL = $(div).children('ul'),
+				imgs = carouselUL.find('img'),
+				imgWidth = 256,
+				imgsLen = imgs.length,
+				current = 1,
+				totalImgsWidth = imgsLen * imgWidth;
+
+				console.log(imgWidth)
+
+			$('#carousel-nav').find('button').on('click', function() {
+				var direction = $(this).data('dir'),
+					loc = imgWidth;
+
+				( direction === 'next' ) ? ++current : --current;
+
+				if ( current === 0 ) {
+					current = imgsLen;
+					loc = imgWidth - totalImgsWidth;
+					direction === 'next';
+				} else if ( current - 1 === imgsLen ) {
+					current = 1;
+					loc = 0;
+				}
+
+				transition(carouselUL, loc, direction);
+			});
+
+			function transition( container, loc, direction ) {
+				var unit;
+
+				if ( direction && loc !== 0 ) {
+					unit = ( direction === 'next' ) ? '-=' : '+=';
+				}
+
+				container.animate({
+					'margin-left': unit ? (unit + loc) : loc
+				});
+			} 			
+		});       
     }
 
-    // Append Stylesheets to head
-    function appendStylesheets() {
-        var stylesheets = [
-            "<link rel='stylesheet' href='/css/bootstrap.min.css' type='text/css' media='screen'>",
-            "<link rel='stylesheet' href='/css/carousel.css' type='text/css' media='screen'>"
-        ];
-        $.each(stylesheets,function() {
-            $("head").append(this);
-        });
+    // Append Stylesheet to head
+    function appendStylesheet() {
+        $("head").append("<link rel='stylesheet' href='/css/sweetCarousel.css'>");
     }
 
     // Putting our plugin in the sweetCarousel Namespace
     $.fn.sweetCarousel = function( options ) {
+    	var div = this;
+    	// override defaults with passed in options
         var opts = $.extend( {}, $.fn.sweetCarousel.defaults, options );
 
-        appendStylesheets();
-        getTemplateAjax('js/templates/sweetCarousel.html', opts);
-
-/*        $('#sweetCarousel').carousel({
-            interval: 1000
-        });*/
-
+        appendStylesheet();
+        getTemplateAjax('js/templates/sweetCarousel.html', opts, div);
     };
 
     // create some defaults
     $.fn.sweetCarousel.defaults = {
          slides: [
             {
-                caption: "Space Invaders: Press Start",
-                image: 'http://dzmgxxm1t5tb7.cloudfront.net/invader.jpg'
+                caption: "Chocolate Cake 1",
+                image: 'http://dzmgxxm1t5tb7.cloudfront.net/chocolate_cake_01.png'
             },
             {
-                caption: "Space Invaders: Full Invasion",
-                image: 'http://dzmgxxm1t5tb7.cloudfront.net/full_invasion.jpg'
+                caption: "Chocolate Cake 2",
+                image: 'http://dzmgxxm1t5tb7.cloudfront.net/chocolate_cake_02.png'
             },
             {
-                caption: "Pac Man",
-                image: 'http://dzmgxxm1t5tb7.cloudfront.net/pacMan.jpg'
+                caption: "Chocolate Cake 3",
+                image: 'http://dzmgxxm1t5tb7.cloudfront.net/chocolate_cake_03.png'
             },
             {
-                caption: "Radiant 1",
-                image: 'http://dzmgxxm1t5tb7.cloudfront.net/radiant1.jpg'
+                caption: "Chocolate Cake 4",
+                image: 'http://dzmgxxm1t5tb7.cloudfront.net/chocolate_cake_04.png'
             },
             {
-                caption: "Radiant 2",
-                image: 'http://dzmgxxm1t5tb7.cloudfront.net/radiant2.jpg'
+                caption: "Chocolate Cake 5",
+                image: 'http://dzmgxxm1t5tb7.cloudfront.net/chocolate_cake_05.png'
+            },
+            {
+                caption: "Chocolate Cake 6",
+                image: 'http://dzmgxxm1t5tb7.cloudfront.net/chocolate_cake_06.png'
             }
         ]
     };
 
-}(jQuery));
+}(jQuery));	
